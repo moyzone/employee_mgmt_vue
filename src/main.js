1953a873f0c4db8ea9991b2f2be9d139662947a3
sourav39 <sourav@onesilq.com>
2022-06-21 18:01:38 +0530

    second commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
-createApp(App).mount('#app')
+const Home = { template: '<div>Hello dude</div>' }
+const About = { template: '<div>About</div>' }
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+const app = createApp(App)
+
+  
+app.use(router)
+
+app.mount('#app')
+
+// createApp(App).mount('#app')
